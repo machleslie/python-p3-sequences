ce87b0f80e0847b3cc6919f88d159861b0ab9cee
machleslie <machariales@gmail.com>
2023-11-23 15:45:51 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    scheme =[]        
+    c = -2
+    a = -1
+    b = 1
+    while len(scheme) < length:
+        c = a+b 
+        a = b
+        b = c
+        scheme.append(c)
     pass
+
+    print(scheme)
+
+print_fibonacci(1)
